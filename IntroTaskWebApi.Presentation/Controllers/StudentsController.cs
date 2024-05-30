a10939b6d91b5cfd892d314cca5489c77082b7d0
Ianina Voda <yaninavoda@gmail.com>
2024-05-30 23:14:37 +0300

    add swagger documentation

--- IntroTaskWebApi.Presentation/Controllers/StudentsController.cs
+++ IntroTaskWebApi.Presentation/Controllers/StudentsController.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
 using Shared.Dtos;
 
 namespace IntroTask.Presentation.Controllers;
 
-[Route("api/[controller]/[action]")]
+[Route("api/[controller]")]
 [ApiController]
+[ApiExplorerSettings(GroupName = "v1")]
 public class StudentsController : ControllerBase
 {
     private readonly IServiceManager _service;
@@ -15,14 +17,23 @@ public class StudentsController : ControllerBase
         _service = service;
     }
 
+    /// <summary>
+    /// Gets the list of all students
+    /// </summary>
+    /// <returns>A list of all students.</returns>
     [HttpGet]
     public async Task<IActionResult> GetStudents()
     {
-        var companies = await _service.StudentService.GetAllStudentsAsync(trackChanges: false);
+        var students = await _service.StudentService.GetAllStudentsAsync(trackChanges: false);
 
-        return Ok(companies);
+        return Ok(students);
     }
 
+    /// <summary>
+    /// Gets the student with the provided id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>The student with the provided id from the database.</returns>
     [HttpGet("{id:int}", Name = "StudentById")]
     public async Task<IActionResult> GetStudent(int id)
     {
@@ -31,11 +42,27 @@ public class StudentsController : ControllerBase
         return Ok(student);
     }
 
+    /// <summary>
+    /// Creates a new student with the data from the request body
+    /// </summary>
+    /// <remarks>
+    /// Sample request
+    /// POST api/students
+    /// {
+    ///     "firstName": "Jane",
+    ///     "lastName": "Doe"
+    /// }
+    /// </remarks>
+    /// <param name="student"></param>
+    /// <returns></returns>
     [HttpPost]
+    [ProducesResponseType(201)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(422)]
     public async Task<IActionResult> CreateStudent([FromBody]StudentCreateDto student)
     {
         if (student is null)
-            return BadRequest("StudentCreatDto object is null");
+            return BadRequest("StudentCreateDto object is null");
 
         if (!ModelState.IsValid)
             return UnprocessableEntity(ModelState);
@@ -46,11 +73,25 @@ public class StudentsController : ControllerBase
         createdStudent);
     }
 
+    /// <summary>
+    /// Updates the student with the provided id and data supplied in the request body
+    /// </summary>
+    /// <remarks>
+    /// Sample request
+    /// PUT api/students
+    /// {
+    ///     "firstName": "Jane",
+    ///     "lastName": "Doe"
+    /// }
+    /// </remarks>
+    /// <param name="id"></param>
+    /// <param name="student"></param>
+    /// <returns></returns>
     [HttpPut("{id:int}")]
     public async Task<IActionResult> UpdateStudent(int id, [FromBody]StudentUpdateDto student)
     {
         if (student is null)
-            return BadRequest("StudentCreatDto object is null");
+            return BadRequest("StudentCreateDto object is null");
 
         if (!ModelState.IsValid)
             return UnprocessableEntity(ModelState);
@@ -60,6 +101,11 @@ public class StudentsController : ControllerBase
         return NoContent();
     }
 
+    /// <summary>
+    /// Deletes the student with the provided id from the database
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     [HttpDelete("{id:int}")]
     public async Task<IActionResult> DeleteStudent(int id)
     {
--- IntroTaskWebApi/Program.cs
+++ IntroTaskWebApi/Program.cs
@@ -2,6 +2,7 @@ using NLog;
 using IntroTaskWebApi;
 using IntroTask.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
@@ -24,8 +25,31 @@ builder.Services.AddControllers()
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Course API",
+        Version = "v1",
+        Description = "Course - teacher - student API",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Ianina Voda",
+            Email = "myemail@gmail.com",
+            Url = new Uri("https://twitter.com/myfacebook"),
+        }
+    });
+    var xmlFile = $"{typeof(IntroTask.Presentation.AssemblyReference).Assembly.GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
 
+    c.IncludeXmlComments(xmlPath);
+});
+    
+
+    
+    
+    
 var app = builder.Build();
 
 app.UseExceptionHandler(opt => { });
@@ -33,7 +57,10 @@ app.UseExceptionHandler(opt => { });
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "Course API V1");
+    });
 }
 
 app.UseHttpsRedirection();
