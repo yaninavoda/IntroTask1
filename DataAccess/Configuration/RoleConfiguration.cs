85ea2305e51aebf60a7af2e8ba32531e7e1740d3
Ianina Voda <yaninavoda@gmail.com>
2024-06-08 12:57:38 +0300

    add RoleConfiguration

---
+++ DataAccess/Configuration/RoleConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Configuration
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+              new IdentityRole
+              {
+                  Name = "Manager",
+                  NormalizedName = "MANAGER"
+              },
+              new IdentityRole
+              {
+                  Name = "Admin",
+                  NormalizedName = "ADMIN"
+              }
+            );
+        }
+    }
+}
---
+++ DataAccess/Migrations/20240608095626_AddedRolesToDb.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRolesToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "8c2ff695-8346-47e8-8653-a9c1b055b5d4", null, "Manager", "MANAGER" },
+                    { "a71ee314-4fef-4807-8cad-64e179753a70", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8c2ff695-8346-47e8-8653-a9c1b055b5d4");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a71ee314-4fef-4807-8cad-64e179753a70");
+        }
+    }
+}
